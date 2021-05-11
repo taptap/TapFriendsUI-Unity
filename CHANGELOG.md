d20d93e144be1c5d49fcd17f71437bfc982dcfb2
zhangwenyan <zhangwenyan@xd.com>
2021-05-11 18:02:55 +0800

    feat(*): update framework

---
+++ CHANGELOG.md
@@ -0,0 +1,19 @@
+# ChangeLog
+
+## 2.1.1
+
+### Dependencies
+
+* TapTap.Bootstrap v2.1.1
+* TapTap.Common v2.1.1
+
+## 2.1.0
+
+### Feature
+
+* Friends
+
+### Dependencies
+
+* TapTap.Bootstrap v2.1.0
+* TapTap.Common v2.1.0
---
+++ Plugins/iOS/TapFriendUISDK.framework/Headers/TapFriendUI.h
@@ -0,0 +1,20 @@
+//
+//  TapFriendUI.h
+//  TapFriendUISDK
+//
+//  Created by Bottle K on 2021/5/11.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+#define TapFriendUISDK @"TapFriendUI"
+#define TapFriendUISDK_VERSION_NUMBER @"20101001"
+#define TapFriendUISDK_VERSION        @"2.1.1"
+
+@interface TapFriendUI : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
