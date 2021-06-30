c13863d7b59dcf8ea743b8ebbc54a12c68da7ac3
xiaoyi <xiaoyi@xd.com>
2021-06-30 14:54:23 +0800

    feat: udpate 2.1.6 binary , env switch to rnd

--- Plugins/iOS/TapFriendUISDK.framework/Headers/TapFriendUI.h
+++ Plugins/iOS/TapFriendUISDK.framework/Headers/TapFriendUI.h
@@ -10,8 +10,8 @@
 NS_ASSUME_NONNULL_BEGIN
 
 #define TapFriendUISDK @"TapFriendUI"
-#define TapFriendUISDK_VERSION_NUMBER @"20105001"
-#define TapFriendUISDK_VERSION        @"2.1.5"
+#define TapFriendUISDK_VERSION_NUMBER @"20104002"
+#define TapFriendUISDK_VERSION        @"2.1.4"
 
 @interface TapFriendUI : NSObject
 
